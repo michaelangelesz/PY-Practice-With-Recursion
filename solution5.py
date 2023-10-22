c7fb44871b212d371731e04592ff9fdc955ca13f
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-10-22 12:33:08 -0400

    check whether a string is a palindrome or not

--- solution5.py
+++ solution5.py
@@ -1,1 +1,19 @@
-# Write code for algorithm 5 below
+# Write a function that checks whether a string is a palindrome or not. The program should take in a string and return True if the string is a palindrome and False if not.
+
+# A palindrome is a word that is the same when it is reversed, such as madam, radar, kayak, or tacocat.
+
+def isPalindrome(word):
+    
+    # base case
+    if len(word) < 2:
+        return True 
+    
+    else: 
+        if word[0] == word[-1]:
+            return isPalindrome(word[1:-1])
+        else: 
+            return False
+
+
+sample_word = 'aibohphobia' # aibohphobia is the fear of palindromes
+print(isPalindrome(sample_word))
