96798bf2b19c55ce0707e788ff2f6e43810e174e
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-10-22 12:31:42 -0400

    prints the natural numbers from 1 to n

--- solution2.py
+++ solution2.py
@@ -1,1 +1,11 @@
-# Write code for algorithm 2 below
+# Write a function that prints the natural numbers from 1 to n.
+
+def natural_numbers(lowerNum, higherNum):
+    if lowerNum > higherNum:
+        return
+    else:
+        print(lowerNum)
+        natural_numbers(lowerNum + 1, higherNum)
+
+n=10
+natural_numbers(1,n)
