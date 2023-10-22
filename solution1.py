9a4e2dc81c6f7a0abf8fcc29ff8e3f4c8a3d19f2
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-10-22 12:31:17 -0400

    print numbers from n to zero

--- solution1.py
+++ solution1.py
@@ -1,4 +1,5 @@
 # Write a program that takes a positive number as an argument and prints the numbers from n to zero.
+
 # Before we begin coding, we want to break down this problem and think about it recursively.
 # We want to define a function that takes a number, let's say n , prints it, and calls itself again with the value of n-1. The function will keep calling itself until the base case is met. One way to do this is to determine if the number is equal to 0 and when it is, stop the function. Think about what the base case and recursive case would be.
 
